2823fc7ce7e9ad42c3f805b0a7d6aa434428881e
Kyle Stewart <kyle.w.stewart@mac.com>
2017-03-25 22:55:40 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+# require 'json'
+#
+# def hash
+#   JSON.parse(File.read('spec/fixtures/contestants.json'))
+# end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name = ""
+  data[season].each do |dict|
+    name = dict["name"] if dict["status"] == "Winner"
+  end
+  name.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |dict|
+      return dict["name"] if dict["occupation"] == occupation
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |dict|
+      count += 1 if dict["hometown"] == hometown
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |dict|
+      return dict["occupation"] if dict["hometown"] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data[season].each do |dict|
+    str_age = dict["age"]
+    int_age = str_age.to_f
+    age_array.push(int_age)
+  end
+  sum = 0
+  age_array.each do |x|
+    sum = sum + x
+  end
+  avg = (sum / age_array.count).round
 end
+
+# data = hash
+# season = "season 10"
+# puts get_average_age_for_season(data, season)
